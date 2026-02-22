0c944b69d0ee972fdfae55e9cf504f9a15d737ad
raiatharva-invictus <raiatharva24.in@gmail.com>
2026-02-22 09:53:32 +0530

    integrated sigup(register) and login to frontend

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,25 @@
 from flask_cors import CORS
-from flask import Flask
+from flask import Flask, Blueprint
 from config import CORS_ORIGINS
 from routes.auth import auth_bp
 from database import init_db
 
+
 app = Flask(__name__)
+CORS(app, origins=CORS_ORIGINS)
 
-CORS(app, origins=[CORS_ORIGINS])
+api_bp = Blueprint('api', __name__, url_prefix='/api')
 
-app.register_blueprint(auth_bp)
+app.register_blueprint(auth_bp, url_prefix='/api/auth')
 
 init_db()
 
-#will import the BKT logic after working on it.
 
 @app.route('/mastery/update', methods=['POST'])
 def update_mastery():
-    #the main simulation for BKT, updating the user mastery parameters.
     pass
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=8080)
 
 
--- backend/config.py
+++ backend/config.py
@@ -3,4 +3,4 @@ from dotenv import load_dotenv
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
-CORS_ORIGINS = ["http://localhost:3000"]
+CORS_ORIGINS = ["http://localhost:5173"]
